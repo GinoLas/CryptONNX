01de60c6ed1cfd93d5ffee4b75e4a34c40336b91
GinoLas2001 <giower.gb@gmail.com>
2026-04-28 18:45:39 +0200

    [ADD]Added hmac generation

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import onnx_graphsurgeon as gs
 import numpy as np
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+import hmac
+import hashlib
 import os
 
 graph = gs.import_onnx(onnx.load("files/network_og.onnx"))
 
 key = bytes.fromhex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")  
+hmac_key = bytes.fromhex("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") 
 nonce = bytes.fromhex("000000000000000000000000CACACACA") 
 backend = default_backend()
 
@@ -34,17 +37,24 @@ for node in graph.nodes:
                         shape = input.values.shape
                         weights_tensor = np.array(input.values,dtype = np.int8)
                         weights_tensor = weights_tensor.transpose(0,2,3,1)
-                        for i, byte in enumerate(weights_tensor.flatten()):
-                            print(str(byte),end="\t")
+
+                        # for i, byte in enumerate(weights_tensor.flatten()):
+                        #     print(str(byte),end="\t")
                         
-                            if (i + 1) % 8 == 0:
-                                print(",")  
-                            else:
-                                print(", ",end="") 
+                        #     if (i + 1) % 8 == 0:
+                        #         print(",")  
+                        #     else:
+                        #         print(", ",end="") 
                         encrypted_bytes = encrypt_weights(weights_tensor)
-                        encrypted_bytes.shape = weights_tensor.shape
+                        encrypted_bytes = encrypted_bytes.reshape(weights_tensor.shape)
+                        digest = hmac.new(hmac_key, np.ascontiguousarray(encrypted_bytes), hashlib.sha256).digest()
+                        hmac_array = np.frombuffer(digest,dtype=np.uint32)
                         input.values = np.array(encrypted_bytes.transpose(0,3,1,2),dtype = np.int8)
-                        input.values.shape = shape
+                        input.values = input.values.reshape(shape)
+                        node.attrs['hmac'] = (gs.Constant(
+                            name = f"{node.name}_hmac",
+                            values = hmac_array
+                        ))
 
                     elif(".mul" in input.name or ".add" in input.name):
                         print("Beh... non ho idea di cosa si possa fare...")
@@ -52,21 +62,23 @@ for node in graph.nodes:
 
             
                     elif("MatMul" in input.name):
-                        print(f"{input.name} --> mat mul constant")
+                        # print(f"{input.name} --> mat mul constant")
                         shape = input.values.shape
                         weights_tensor = np.array(input.values,dtype = np.int8)
                         weights_tensor = weights_tensor.transpose(1,0)
-                        for i, byte in enumerate(weights_tensor.flatten()):
-                            print(str(byte),end="\t")
+                        # for i, byte in enumerate(weights_tensor.flatten()):
+                        #     print(str(byte),end="\t")
                         
-                            if (i + 1) % 8 == 0:
-                                print(",")  
-                            else:
-                                print(", ",end="") 
+                        #     if (i + 1) % 8 == 0:
+                        #         print(",")  
+                        #     else:
+                        #         print(", ",end="") 
                         encrypted_bytes = encrypt_weights(weights_tensor)
                         encrypted_bytes.shape = weights_tensor.shape
                         input.values = np.array(encrypted_bytes.transpose(1,0),dtype = np.int8)
                         input.values.shape = shape
+        
+
 
                 
 graph.cleanup().toposort()
