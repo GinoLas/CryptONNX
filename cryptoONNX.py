5280442a4505b11a31cd83b4b07e9b4bce70e405
GinoLas2001 <giower.gb@gmail.com>
2026-04-30 00:16:50 +0200

    Removed shape change due to integration of the tool in Deeploy lowering flow

--- cryptoONNX.py
+++ cryptoONNX.py
@@ -23,42 +23,52 @@ class ONNXEncryptor:
 
     def process_graph(self, graph: gs.Graph) -> gs.Graph:
         """
-        Prende un oggetto gs.Graph, cifra i pesi in-place e aggiunge gli attributi HMAC.
+        Prende un oggetto gs.Graph, cifra i pesi e aggiunge come attributo ai nodi HMAAC sui pesi.
         """
         for node in graph.nodes:
             if len(node.inputs) > 1:
-                for inp in node.inputs:
-                    if isinstance(inp, gs.Constant):
+                print(f"Processing node {node.name}")
+                for input in node.inputs:
+                    print(input.name)
+                    if isinstance(input, gs.Constant):
                         
                         # Weights 
-                        if ".weight" in inp.name:
-                            shape_orig = inp.values.shape
+                        if "weight_tensor" in input.name:
+                            shape = input.values.shape
+                            weights_tensor = np.array(input.values,dtype = np.int8)
+                            # weights_tensor = weights_tensor.transpose(0,2,3,1)
 
-
-                            weights_tensor = np.array(inp.values, dtype=np.int8).transpose(0, 2, 3, 1)
+                            # for i, byte in enumerate(weights_tensor.flatten()):
+                            #     print(str(byte),end="\t")
                             
+                            #     if (i + 1) % 8 == 0:
+                            #         print(",")  
+                            #     else:
+                            #         print(", ",end="") 
                             encrypted_bytes = self._encrypt_weights(weights_tensor)
                             encrypted_bytes = encrypted_bytes.reshape(weights_tensor.shape)
-                            
                             digest = hmac.new(self.hmac_key, np.ascontiguousarray(encrypted_bytes), hashlib.sha256).digest()
-                            hmac_array = np.frombuffer(digest, dtype=np.uint32)
-                            
-                            inp.values = np.array(encrypted_bytes.transpose(0, 3, 1, 2), dtype=np.int8).reshape(shape_orig)
-                            
-                            node.attrs['hmac'] = gs.Constant(name=f"{node.name}_hmac", values=hmac_array)
+                            hmac_array = np.frombuffer(digest,dtype=np.uint32)
+                            # input.values = np.array(encrypted_bytes.transpose(0,3,1,2),dtype = np.int8)
+                            input.values = np.array(encrypted_bytes,dtype = np.int8)
+                            input.values = input.values.reshape(shape)
+                            node.attrs['hmac'] = (gs.Constant(
+                                name = f"{node.name}_hmac",
+                                values = hmac_array
+                            ))
 
                         # MatMul constants
-                        elif "MatMul" in inp.name or (hasattr(inp, 'name') and "fc" in inp.name):
-                            shape_orig = inp.values.shape
-                            weights_tensor = np.array(inp.values, dtype=np.int8).transpose(1, 0)
+                        elif "MatMul" in input.name or (hasattr(input, 'name') and "fc" in input.name):
+                            og_shape = input.values.shape
+                            weights_tensor = np.array(input.values, dtype=np.int8).transpose(1, 0)
                             
                             encrypted_bytes = self._encrypt_weights(weights_tensor)
                             encrypted_bytes.shape = weights_tensor.shape
                             
-                            inp.values = np.array(encrypted_bytes.transpose(1, 0), dtype=np.int8).reshape(shape_orig)
+                            input.values = np.array(encrypted_bytes.transpose(1, 0), dtype=np.int8).reshape(og_shape)
 
                         # Biases
-                        elif ".mul" in inp.name or ".add" in inp.name:
+                        elif ".mul" in input.name or ".add" in input.name:
                             # Not handled right now
                             pass
 
